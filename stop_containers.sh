3b90f77469a069f3b373af830b3eb2ac89c14f51
vaibhavpandey1 <vaibhavpandey@cyntexa.com>
2024-11-20 23:24:50 +0530

    night 11

--- stop_containers.sh
+++ stop_containers.sh
@@ -1,19 +1,4 @@
-# #!/bin/bash
-
-# # Stop all running containers
-
-# cd /home/ec2-user/
-# docker-compose down
-
 #!/bin/bash
-
-# Stop all running containers using docker-compose
-
-# Navigate to the directory where the docker-compose.yml file is located
+# Stop all running containers
 cd /home/ec2-user/
-
-# Stop running containers
-echo "Stopping containers..."
-docker-compose down || { echo "Failed to stop containers"; exit 1; }
-
-echo "Containers stopped successfully."
+docker-compose down
